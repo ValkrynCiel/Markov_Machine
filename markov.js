3dddb464d62bb398f0866ccd2076f9a73f5c9ef0
Ethan Shen <ethanshen.wu@gmail.com>
2019-03-05 11:17:04 -0800

    created chains

--- markov.js
+++ markov.js
@@ -12,13 +12,34 @@ class MarkovMachine {
   }
 
   /** set markov chains:
-   *
+   *  ["the", "cat", "in", the", hat]
    *  for text of "the cat in the hat", chains will be
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    // iterate through this.words, such that i is set to key and i+1 is pushed into list of words
+    let markovChains = {};
+    let words = this.words;
+
+    for (let i = 0; i < words.length; i++){
+    
+      // if there is no value set, create array, else push word into array
+      if (markovChains[words[i]]){
+
+        markovChains[words[i]].push(words[i+1]);
+        
+      } else {
+
+        markovChains[words[i]] = [];
+        markovChains[words[i]].push(words[i+1]);
+        
+      }
+      // markovChains[words[i]] = markovChains[words[i]] ? markovChains[words[i]].push(words[i+1]) : new Array().push(words[i])
+      
+    }
+    console.log(markovChains);
   }
+  
 
 
   /** return random text from chains */
@@ -27,3 +48,4 @@ class MarkovMachine {
     // TODO
   }
 }
+let test = new MarkovMachine("The cat in the hat the cat in the hat");
